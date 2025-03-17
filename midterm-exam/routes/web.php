9f15723afccc7aae2eb4c50592ec1877a54822c7
TERENZE DANE C. MAGRACIA <2200612@cc.usls.edu>
2025-03-17 14:52:05 +0800

    Lastname_MidtermExam

--- midterm-exam/routes/web.php
+++ midterm-exam/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FeatureController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/MagraciaTerenzeDane', [FeatureController::class, 'index']);
